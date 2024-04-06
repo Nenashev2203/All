0a161d976b87444937aec906fca9840b8f68b69d
Nenashev2203 <nenandrey@gmail.com>
2024-04-06 11:09:06 +0400

    12 version 2

--- lesson1/20.py
+++ lesson1/20.py
@@ -2,9 +2,10 @@ x=float(input())
 n=-1
 a=0.00000001
 rez=1
-b=0
+s=0
 while abs(rez)>a:
-    n+= 1
-    rez =(((-1)**(n))*(x**(n+1))/(n+1)
-    b+= rez
+    n += 1
+    rez = (((-1)**(n))*(x**(n+1))/(n+1)
+    s += rez
+
 print(round(s, 8))
