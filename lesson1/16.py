4a533d25edb224afd37013538f1c1cbc9250b528
Nenashev2203 <nenandrey@gmail.com>
2024-04-06 08:25:40 +0400

    12

--- lesson1/16.py
+++ lesson1/16.py
@@ -1,6 +1,6 @@
 import math
-x=int(input("x= )))
-y=int(input("y= )))
+x=int(input("x= "))
+y=int(input("y= "))
 
 r1=2
 r2=1
@@ -10,7 +10,7 @@ b = math.sqrt((x-1)**2+y**2);
 
 if a<=r1 and b>=r2 :
     print("yes", end=" ")
-elif 2<=x<=6 and -1<=y<=3
+elif 2<=x<=6 and -1<=y<=3 :
     print("yes")
 else:
     print("no")
